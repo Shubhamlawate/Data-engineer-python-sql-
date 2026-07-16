cfd64637744e75a7c23b41320481cc6aff5e9309
Shubham_MDM-project <lavateshubham11@gmail.comm>
2026-07-17 00:36:02 +0530

    Add string.py

---
+++ python2/string.py
@@ -0,0 +1,32 @@
+# str_1 = 'python'## Single quotes
+# str="python"#double quotes
+# str3='''python'''#triple quotes
+
+# string indexing
+
+# str ="python" 
+# print(str[0])
+# print(str[1])
+# print(str[-1])
+# print(str[-2])
+
+#string slcising
+# print(str[:4])
+# print(str[0:])
+
+
+# methods 
+
+# text=" PYTHON"
+# print(text.upper())
+
+
+# print(text.lower())
+
+name = "shubham lawate"
+# print(name.title())
+
+# print(name.capitalize())
+
+# print(name.swapcase())
+print(name. strip())

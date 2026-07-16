caeb63e64a4a43af3aca5133992fa5a48c5904f1
Shubham_MDM-project <lavateshubham11@gmail.comm>
2026-07-16 22:26:10 +0530

    Add list.py

---
+++ python2/list.py
@@ -0,0 +1,34 @@
+# completed method of lists
+num = [1,2,3,4,5,4,4] 
+# print(num)
+# num.append(8)
+# print(num )
+# num.extend([ 6,7,8,0])
+# print(num)
+# num.insert(2,3) # imp for ads
+# num.insert(2,2)
+# print(num)
+# num.remove(3)
+# num.remove(2)
+# print(num)
+# x= num.pop()
+# x=num.pop()
+# x=num.pop()
+# print(x)
+# print(num)
+
+# print(num)
+# print(num.index(3)) # this numb reture index
+
+# print(num.count(4))
+
+# num.sort()
+# print(num )
+# num.sort(reverse=True)
+# print(num)
+
+# num.reverse()
+# print(num)
+
+# num1=num.copy()
+# print(num1)

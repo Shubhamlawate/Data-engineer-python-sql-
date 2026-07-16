ff78cfcde6d25ad69eac2edfd142384655b0dc4f
Shubham_MDM-project <lavateshubham11@gmail.comm>
2026-07-16 23:20:39 +0530

    Add set.py

---
+++ python2/set.py
@@ -0,0 +1,36 @@
+# method of sets
+set ={1,2,3,4,5,6,7,8,9,0}
+
+# set.add(45)
+# print(set)
+
+# set.update( [21,32,33])
+# print(set)
+
+# set.remove(4)
+# print(set) 
+
+# set.discard(31)
+# print(set)
+# x=set.pop()
+# print(x)
+# print(set)
+
+
+# set.clear()
+# print(set)
+
+# x_=set.copy()
+# print(x_)
+
+#opretion sets
+
+a ={1,2,3}
+b={4,5,6}
+# print(a|b)  union
+
+# print(a&b)    # intersection
+
+# print(a-b)  #differes
+
+# print(a^b)

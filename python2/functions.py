399193e7fc86bda21604547bbbd02687250288ce
Shubham_MDM-project <lavateshubham11@gmail.comm>
2026-07-17 11:34:08 +0530

    Add functions.py

---
+++ python2/functions.py
@@ -0,0 +1,73 @@
+# function syntax
+
+# def guset():
+#     # function body
+#     return value
+
+
+#function type
+
+#Without parameters
+
+# def welcome():
+#     print("hello guset")
+
+# welcome()
+
+#with parameter
+
+# def add(a,b):
+#     print(a+b)
+
+# add(3,5)
+
+#with return 
+
+# def add(a,b):
+#     return a+b
+# result=add(10,12)
+# print(result)
+
+#with  default
+# def greet(name="guset"):
+#     print("hello",name)
+
+# greet()
+# greet("shubham")
+#
+#keyword argument
+# def student(name,age):
+#     print(name,age)
+
+# student("shubham",19)
+
+#varible length argument
+# def total(*number):
+#     print(sum(number))
+# total(12,3,4,5,465,6)
+
+#keyword varible length argument
+
+# def student(**detila):
+#     print(detila)
+
+# student(name="sub",age=12)
+
+#lambda function  
+
+ #syntax 
+#lambda argument : expresion
+
+# squre =lambda x:x*x
+# print(squre(5))
+
+#recursive function this function itself call
+# def factorial(n):
+#     if n==1:
+#         return 1
+#     return n*factorial(n-1)
+# print(factorial(5))
+
+
+ 
+

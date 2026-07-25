9e5532467b1399883058ec2530bc77961e984a8a
Shubham Lavate <lavateshubham11@gmail.com>
2026-07-25 21:42:37 +0530

    Add all_pandas.py

---
+++ python2/all_pandas.py
@@ -0,0 +1,40 @@
+# import pandas as pd
+
+# s = pd.Series([10,20,30,40,50,60],name="mark")
+# print(s)
+#Mathmatic function
+# s = pd.Series([10,20,30,40,50,60],name="mark")
+#Mathmatic function
+# print(s+5)#add
+# print(s-5)#subtrac
+# print(s/5)#"divide"
+# print(s*2)# multipecation
+# print(s**2)
+# Arthmatic function
+# print(s.sum())
+# print(s.min())
+# print(s.max())
+# print(s.count())
+# print(s.std())
+ 
+
+
+# s_var =pd.Series([70,80,90,40,50,60],["a","b","c","d","e","f"])
+# print(s_var)
+
+
+# DATAFRAME
+# import pandas as pd
+
+# Data ={
+#     "name":["shubham","karan","rashed","bhupathi"],
+#     "age":[20,21,21,30],
+#     "city":["pandherpur","vasai","bhiwande","kerala"],
+#     "college":"vcet",
+#     "mark":[80,89,90,99]
+
+# }
+
+# df = pd. DataFrame(Data)
+# print(df.tail())
+# print(df.head())
